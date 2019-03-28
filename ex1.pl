80d15a5332d61584840b245c818f5b9496a3629e
arturomf94 <arturomf94@gmail.com>
2019-03-28 11:35:13 -0600

    add exs

---
+++ ex1.pl
@@ -0,0 +1,3 @@
+f(X,verde) :- X < 3, !.
+f(X,amarilla) :- X < 6, !.
+f(_, roja).
---
+++ ex2.pl
@@ -0,0 +1,7 @@
+estrellas(diverxo).
+estrellas(ricard_camarena).
+
+caro(diverxo).
+
+razonable(X) :-
+  \+ caro(X).
